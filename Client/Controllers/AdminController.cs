76ae1be80a822028d07cfac7bd2ed8216c595848
andre <skiter.andre@gmail.com>
2021-02-08 20:41:06 -0300

    Adicionar arquivos de projeto.

---
+++ Client/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public IActionResult Secure()
+        {
+            return View();
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
+        }
+    }
+}
